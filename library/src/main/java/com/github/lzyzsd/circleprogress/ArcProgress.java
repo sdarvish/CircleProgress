efccd9c002fffd1b974ecadb8bf9b34defe6afe3
Saeed Darvish <sd.saeed.darvish@gmail.com>
2021-08-18 13:02:17 +0430

    #147 fixed - Disable Default Suffix

--- library/src/main/java/com/github/lzyzsd/circleprogress/ArcProgress.java
+++ library/src/main/java/com/github/lzyzsd/circleprogress/ArcProgress.java
@@ -94,7 +94,7 @@ public class ArcProgress extends View {
         default_text_size = Utils.sp2px(getResources(), 40);
         default_suffix_text_size = Utils.sp2px(getResources(), 15);
         default_suffix_padding = Utils.dp2px(getResources(), 4);
-        default_suffix_text = "%";
+        default_suffix_text = "";
         default_bottom_text_size = Utils.sp2px(getResources(), 10);
         default_stroke_width = Utils.dp2px(getResources(), 4);
 
